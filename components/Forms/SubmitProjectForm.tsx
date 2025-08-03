4d95f9ec39c05cb3689917b7f3038a925a5a965c
Harshalvk <harshalvkhobragade@gmail.com>
2025-08-04 00:09:10 +0530

    feat: form for submitting project

---
+++ components/Forms/SubmitProjectForm.tsx
@@ -0,0 +1,23 @@
+import { submitProjectSchema } from "@/utils/validator/student.validator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export type FormTypeSubmitProject = z.infer<typeof submitProjectSchema>;
+
+export const useFormSubmitProject = () =>
+  useForm<FormTypeSubmitProject>({
+    resolver: zodResolver(submitProjectSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      domain: [],
+      githubUrl: "",
+      liveLink: "",
+      pptUrl: "",
+      projectThumbnail: "",
+      synopsis: "",
+      teamMembers: [],
+      technologies: []
+    }
+  });

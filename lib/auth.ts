aa160d71d0828bfd6c90db09041023bb6f4871c3
Harshalvk <harshalvkhobragade@gmail.com>
2025-08-03 15:50:40 +0530

    feat: update auth configs

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,8 +12,11 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string
     },
     google: {
+      prompt: "select_account",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }
   }
 });
+
+export type Session = typeof auth.$Infer.Session;

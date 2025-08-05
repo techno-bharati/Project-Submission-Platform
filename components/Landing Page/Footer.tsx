3541de418dc030fe6f69bd50f44bf8291f488a2e
Harshalvk <harshalvkhobragade@gmail.com>
2025-08-05 21:58:45 +0530

    ui: update footer

--- components/Landing Page/Footer.tsx
+++ components/Landing Page/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full border-t border-dashed dark:border-zinc-700 border-zinc-300 p-2 text-center">
       <p className="text-sm text-muted-foreground">
-        &copy; Infusion AI. All rights reserved.
+        &copy; All rights reserved.
       </p>
     </footer>
   );
